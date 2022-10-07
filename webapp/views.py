2c51549622516f8d48d3dd380ff9506b82594ab4
Bigsteve001 <stephenkingsoft1@gmail.com>
2022-10-07 14:25:55 -0700

    perfected the contact form
    from email is not sending need to fix

--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,13 @@
 from multiprocessing import context
+from pickle import FALSE
 from select import select
 from turtle import home
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from webapp.models import *
 from django.core.mail import send_mail
+import socket
+socket.getaddrinfo('localhost', 8000)
 
 # Create your views here.
 def index(request):
@@ -31,22 +34,20 @@ def index(request):
     if request.method=="POST":
       Form=form()
       name=request.POST.get('name')
-      email=request.POST.get('email')
+      from_email=request.POST.get('email')
       subject=request.POST.get('subject')
       message=request.POST.get('message')
       Form.name=name
-      Form.email=email
+      Form.email=from_email
       Form.subject=subject
       Form.message=message
       Form.save()
 
       send_mail(
          name ,
-         message,
-         email,
-         subject,
-         ['stephenkingsoft1@gmail.com', 'awesomejohnnydepp@gmail.com'],
-      )
+         subject + "\n" + message,
+         from_email,
+         ['stephenkingsoft1@gmail.com'])
       
       return HttpResponse('Thanks For Contacting Us')
       
